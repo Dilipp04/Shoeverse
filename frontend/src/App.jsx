3a44a59d1428037299c127e53580fae24f880618
Dilip Prajapati <dilipp11082004@gmail.com>
2026-09-14 12:29:12 +0530

    Fix the Error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { ShopProvider } from './context/ShopContext'
 import Layout from './components/layout/Layout'
@@ -13,81 +12,16 @@ import AccountPage from './pages/AccountPage'
 import AdminPage from './pages/AdminPage'
 
 export default function App() {
-    return <BrowserRouter><ShopProvider><Layout><Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/shop" element={<CatalogPage />} />
-        <Route path="/products/:slug" element={<ProductPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/checkout" element={<ProtectedRoute><CheckoutPage /></ProtectedRoute>} />
-        <Route path="/login" element={<AuthPage login />} />
-        <Route path="/register" element={<AuthPage />} />
-        <Route path="/account" element={<ProtectedRoute><AccountPage /></ProtectedRoute>} />
-        <Route path="/admin" element={<AdminRoute><AdminPage /></AdminRoute>} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes></Layout></ShopProvider></BrowserRouter>
+  return <BrowserRouter><ShopProvider><Layout><Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/shop" element={<CatalogPage />} />
+    <Route path="/products/:slug" element={<ProductPage />} />
+    <Route path="/cart" element={<CartPage />} />
+    <Route path="/checkout" element={<ProtectedRoute><CheckoutPage /></ProtectedRoute>} />
+    <Route path="/login" element={<AuthPage login />} />
+    <Route path="/register" element={<AuthPage />} />
+    <Route path="/account" element={<ProtectedRoute><AccountPage /></ProtectedRoute>} />
+    <Route path="/admin" element={<AdminRoute><AdminPage /></AdminRoute>} />
+    <Route path="*" element={<Navigate to="/" replace />} />
+  </Routes></Layout></ShopProvider></BrowserRouter>
 }
-=======
-import "./App.css";
-import React, { useState, useEffect } from "react";
-import Home from "./components/Home";
-import Navbar from "./components/Navbar";
-import Cart from "./components/Cart";
-import AddProduct from "./components/AddProduct";
-import Product from "./components/Product";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AppProvider } from "./Context/Context";
-import UpdateProduct from "./components/UpdateProduct";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-function App() {
-  const [cart, setCart] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState("");
-
-  const handleCategorySelect = (category) => {
-    setSelectedCategory(category);
-    console.log("Selected category:", category);
-  };
-  const addToCart = (product) => {
-    const existingProduct = cart.find((item) => item.id === product.id);
-    if (existingProduct) {
-      setCart(
-        cart.map((item) =>
-          item.id === product.id
-            ? { ...item, quantity: item.quantity + 1 }
-            : item
-        )
-      );
-    } else {
-      setCart([...cart, { ...product, quantity: 1 }]);
-    }
-  };
-
-  return (
-    <AppProvider>
-      <BrowserRouter>
-        <Navbar onSelectCategory={handleCategorySelect}
-         />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Home addToCart={addToCart} selectedCategory={selectedCategory}
-              />
-            }
-          />
-          <Route path="/add_product" element={<AddProduct />} />
-          <Route path="/product" element={<Product  />} />
-          <Route path="product/:id" element={<Product  />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/product/update/:id" element={<UpdateProduct />} />
-        </Routes>
-      </BrowserRouter>
-    </AppProvider>
-  );
-}
-
-export default App;
->>>>>>> e53f6e9c670f659c7447e69cce5809d1c20d9862
